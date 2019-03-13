992a3835b9aeb34a3d3906db3efe5377cf0bb56e
Diego E <diego.esgu@gmail.com>
2019-03-12 19:52:57 -0500

    segundo commit

---
+++ src/main/java/com/avatar/diesgut/DiesgutApplication.java
@@ -0,0 +1,13 @@
+package com.avatar.diesgut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiesgutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiesgutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avatar/diesgut/DiesgutApplicationTests.java
@@ -0,0 +1,16 @@
+package com.avatar.diesgut;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiesgutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
